ee6477415f4093804b6b61f84b59549be7646da6
Juanpablofrerscampos <juanpfrers@gmail.com>
2021-05-09 19:29:00 -0300

    guns added

--- packages/gamemode/guns.js
+++ packages/gamemode/guns.js
@@ -10,3 +10,11 @@ mp.events.addCommand('minigun', (player, fullText) => {
 mp.events.addCommand('pistola', (player, fullText) => {
     player.giveWeapon(0xAF3696A1, 9999);
 });
+
+mp.events.addCommand('granada', (player, fullText) => {
+    player.giveWeapon(0x93E220BD, 9999);
+});
+
+mp.events.addCommand('molotov', (player, fullText) => {
+    player.giveWeapon(0x24B17070, 9999);
+});
