da940fbf9ea27fad784212cdf4e015c0e5ec9f4a
Juanpablofrerscampos <juanpfrers@gmail.com>
2021-05-09 17:35:58 -0300

    first approximation to /adminservicio logic

--- packages/gamemode/admin/index.js
+++ packages/gamemode/admin/index.js
@@ -1,6 +1,7 @@
+"use strict"
 mp.events.addCommand("tp", (player, _fulltext, id) => {
     if (!id || isNaN(id)) {
-        return player.outputChatBox(`Usage: /tp <ID>`)
+        return player.outputChatBox(`Usage: /tp <ID>`);
     }
     let targetPlayer = mp.players.at(id);
     if (targetPlayer) {
@@ -8,6 +9,22 @@ mp.events.addCommand("tp", (player, _fulltext, id) => {
         player.outputChatBox(`You teleported to ${targetPlayer.name}`);
     }
     else {
-        player.outputChatBox(`That player doesn't exist`)
+        player.outputChatBox(`That player doesn't exist`);
     };
 });
+
+mp.events.addCommand('adminservicio', (player, fullText) => {
+    //Falta verificar si el usuario es administrador o no.
+
+    //Si no está en servicio, almacena la vida que tiene, luego lo hace inmortal. Si está en servicio, le setea la vida pre-servicio. Hay que almacenar esto en la BDD, y verificar las comprobaciones.
+    /* if (player.enServicio == false){ */
+        let vidaPreServicioAdmin = player.health;
+        let nombreJugador = player.name;
+        player.health = 10000;
+        player.name = `!{0804FF}${player.name}`;
+    /* } */
+/*     else{
+        player.health = vidaPreServicioAdmin;
+        player.name = `!{FFFFFF}${player.name}`;
+    } */
+})
