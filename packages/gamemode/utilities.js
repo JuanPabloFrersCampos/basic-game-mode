a0d20d2d60be2519544d0947487dfd1fdafb8773
Juanpablofrerscampos <juanpfrers@gmail.com>
2021-05-13 08:26:39 -0300

    utilities.js optimized

--- packages/gamemode/utilities.js
+++ packages/gamemode/utilities.js
@@ -12,8 +12,3 @@ mp.events.addCommand('armorme', (player) => {
     player.armour = 100;
 });
 
-mp.events.addCommand('car', (player, vehicleName) => {
-    const model = mp.joaat(vehicleName);
-
-    const veh = mp.vehicles.new(model, player.position, { dimension: player.dimension, heading: player.heading });
-});
